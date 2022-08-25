d9146c04f9283bae1cfb4c492b873df52575beca
Maracujacake <madlordx@gmail.com>
2022-08-25 14:48:29 -0300

    versao 1.0

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './index.html'
+import '../dist/output.css'
+import '../src/input.css'
+const navBtn = document.querySelector('#menu');
+const menuBar = document.querySelector('[role="menubar"]');
+
+navBtn.addEventListener('click', () => {
+  const isExpanded = JSON.parse(navBtn.getAttribute('aria-expanded'));
+  navBtn.setAttribute('aria-expanded', !isExpanded);
+  menuBar.classList.toggle('hidden');
+  menuBar.classList.toggle('flex');
+})
